da3406bc491a6d425bad6ea6140ac33793d99479
Divyahans Gupta <divyahansg@gmail.com>
2013-07-27 10:00:14 -0700

    analytics

--- js/client.js
+++ js/client.js
@@ -95,3 +95,11 @@ window.onbeforeunload = function(){
   if(!submitted)
   	return 'Submit a compliment before you leave!';
 };
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-42775417-1', 'warmfuzzi.es');
+  ga('send', 'pageview');
+
